e7964f825483528cf20536ce28c29922e079e207
Daniyudha <dani.yudha33@gmail.com>
2022-02-13 01:23:29 +0700

    move form

--- resources/views/home/form-pendaftaran.blade.php
+++ resources/views/home/form-pendaftaran.blade.php
@@ -14,7 +14,7 @@
   <!-- main -->
   <main id="main">
     <div class="container mt-5 section-bg p-3 rounded-3">
-      <h5>Pilih Skema Sertifikasi</h5>
+      <h5><span class="text-danger">*</span> Pilih Skema Sertifikasi</h5>
       <select class="form-select" aria-label="Default select example">
         <option selected>-- Pilih Skema --</option>
         <option value="1">1. MENJAHIT ALAS KAKI</option>
@@ -29,20 +29,14 @@
         <option value="10">10. PENYELESAIAN PRODUK ALAS KAKI (FINISHER)</option>
         <option value="11">11. PERAKITAN ALAS KAKI</option>
       </select>
-      <!--<h6 class="mt-3">Jenis Uji</h6>
-      <select class="form-select" aria-label="Default select example">
-          <option selected>-- Pilih Jenis Uji --</option>
-          <option value="1">RCC</option>
-          <option value="2">SERTIFIKASI</option>
-      </select>-->
       <div class="mb-3 mt-3">
-        <p class="form-label fw-bold">Jenis Uji</p>
+        <p class="form-label fw-bold"><span class="text-danger">*</span> Jenis Uji</p>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" required>
           <label class="form-check-label" for="inlineRadio1">SERTIFIKASI</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" required>
           <label class="form-check-label" for="inlineRadio2">RCC</label>
         </div>
       </div>
@@ -53,52 +47,52 @@
         <p class="fw-bold text-white mb-0">Data Diri</p>
       </div>
       <div class="mb-3 mt-3">
-        <label for="exampleFormControlInput1" class="form-label fw-bold">No. KTP :</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="No. KTP">
+        <label for="exampleFormControlInput1" class="form-label fw-bold"><span class="text-danger">*</span> No. KTP :</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="No. KTP" required>
       </div>
       <div class="mb-3 mt-3">
-        <label for="exampleFormControlInput1" class="form-label fw-bold">Nama Lengkap :</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nama Lengkap">
+        <label for="exampleFormControlInput1" class="form-label fw-bold"><span class="text-danger">*</span> Nama Lengkap :</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nama Lengkap" required>
       </div>
       <div class="mb-3 mt-3">
-        <p class="form-label fw-bold">Jenis Kelamin :</p>
+        <p class="form-label fw-bold"><span class="text-danger">*</span> Jenis Kelamin :</p>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" required>
           <label class="form-check-label" for="inlineRadio1">Laki-laki</label>
         </div>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" required>
           <label class="form-check-label" for="inlineRadio2">Perempuan</label>
         </div>
       </div>
       <div class="mb-3 mt-3">
-        <label for="exampleFormControlInput1" class="form-label fw-bold">No. Telepon :</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="No. Telepon yang aktif">
+        <label for="exampleFormControlInput1" class="form-label fw-bold"><span class="text-danger">*</span> No. Telepon :</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="No. Telepon yang aktif" required>
       </div>
       <div class="mb-3 mt-3">
-        <label for="exampleFormControlInput1" class="form-label fw-bold">Email Pribadi :</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <label for="exampleFormControlInput1" class="form-label fw-bold"><span class="text-danger">*</span> Email Pribadi :</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" required>
       </div>
       <div class="mb-3 mt-3">
-        <label for="exampleFormControlInput1" class="form-label fw-bold">Tempat Lahir :</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="No. KTP">
+        <label for="exampleFormControlInput1" class="form-label fw-bold"><span class="text-danger">*</span> Tempat Lahir :</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="No. KTP" required>
       </div>
       <form>
         <div class="form-group">
-          <label for="date" class="form-label fw-bold">Tanggal :</label>
+          <label for="date" class="form-label fw-bold"><span class="text-danger">*</span> Tanggal Lahir :</label>
           <div class="input-group date" id="datepicker">
-            <input type="text" class="form-control">
+            <input type="text" class="form-control" required>
             <span class="input-group-append">
-                            <span class="input-group-text bg-white d-block">
-                                    <i class="fa fa-calendar"></i>
-                            </span>
-                        </span>
+              <span class="input-group-text bg-white d-block">
+                <i class="fa fa-calendar"></i>
+              </span>
+            </span>
           </div>
         </div>
       </form>
       <form class="form-horizontal" method="post">
         <div class="form-group mb-3 mt-3">
-          <label class="form-label fw-bold">Provinsi:</label>
+          <label class="form-label fw-bold"><span class="text-danger">*</span> Provinsi:</label>
           <div>
             <select class="form-control" name="provinsi" id="provinsi">
               <option></option>
@@ -107,7 +101,7 @@
           </div>
         </div>
         <div class="form-group mb-3 mt-3">
-          <label class="form-label fw-bold" >Kota/Kabupaten:</label>
+          <label class="form-label fw-bold" > <span class="text-danger">*</span> Kota/Kabupaten:</label>
           <div>
             <select class="form-control" name="kota" id="kota">
               <option></option>
@@ -116,7 +110,7 @@
           </div>
         </div>
         <div class="form-group mb-3 mt-3">
-          <label class="form-label fw-bold" >Kecamatan:</label>
+          <label class="form-label fw-bold" ><span class="text-danger">*</span> Kecamatan:</label>
           <div>
             <select class="form-control" name="kecamatan" id="kecamatan">
               <option></option>
@@ -125,7 +119,7 @@
           </div>
         </div>
         <div class="form-group mb-3 mt-3">
-          <label class="form-label fw-bold" >Kelurahan:</label>
+          <label class="form-label fw-bold" ><span class="text-danger">*</span> Kelurahan:</label>
           <div>
             <select class="form-control" name="kelurahan" id="kelurahan">
               <option></option>
@@ -134,11 +128,11 @@
         </div>
       </form>
       <div class="mb-3 mt-3">
-        <label for="exampleFormControlInput1" class="form-label fw-bold">Kode Pos :</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="xxxxx">
+        <label for="exampleFormControlInput1" class="form-label fw-bold"><span class="text-danger">*</span> Kode Pos :</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="xxxxx" required>
       </div>
       <div class="mb-3 mt-3">
-        <label for="exampleFormControlTextarea1" class="form-label fw-bold">Alamat Sesuai KTP :</label>
+        <label for="exampleFormControlTextarea1" class="form-label fw-bold"><span class="text-danger">*</span> Alamat Sesuai KTP :</label>
         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
       </div>
       
@@ -146,7 +140,7 @@
         <p class="fw-bold text-white mb-0">Data Pendidikan</p>
       </div>
       <div class="form-group mb-3 mt-3">
-        <label class="form-label fw-bold" >Pendidikan Terakhir :</label>
+        <label class="form-label fw-bold" ><span class="text-danger">*</span> Pendidikan Terakhir :</label>
         <select class="form-select" aria-label="Default select example">
           <option selected>-- Pilih Pendidikan --</option>
           <option value="1">1. S3</option>
@@ -162,16 +156,32 @@
         </select>
       </div>
       <div class="mb-3 mt-3">
-        <label for="exampleFormControlInput1" class="form-label fw-bold">Perguruan Tinggi/Universitas :</label>
+        <label for="exampleFormControlInput1" class="form-label fw-bold"><span class="text-danger">*</span> Perguruan Tinggi/Universitas atau Sekolah :</label>
         <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Lembaga atau Institusi Pendidikan">
       </div>
       <div class="mb-3 mt-3">
-        <label for="exampleFormControlInput1" class="form-label fw-bold">Jenis/Bidang Usaha :</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Bidang Usaha Tempat Praktek">
+        <label for="exampleFormControlInput1" class="form-label fw-bold"><span class="text-danger">*</span> Jenis/Bidang Usaha :</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Bidang Usaha Tempat Praktek" required>
+      </div>
+
+      <div class="bg-sub p-2 mt-4 rounded">
+        <p class="fw-bold text-white mb-0">Unggah Dokumen</p>
+      </div>
+      <div class="mt-3 mb-3">
+        <label class="form-label fw-bold" for="inputGroupFile01"><span class="text-danger">*</span> Upload Foto Ktp</label>
+        <input type="file" class="form-control" id="inputGroupFile01" required>
+      </div>
+      <div class="mt-3 mb-3">
+        <label class="form-label fw-bold" for="inputGroupFile01"><span class="text-danger">*</span> Upload Foto Ijazah</label>
+        <input type="file" class="form-control" id="inputGroupFile01" required>
+      </div>
+      <div class="mt-3 mb-3">
+        <label class="form-label fw-bold" for="inputGroupFile01">Upload Sertifikat Prlatihan (Opsional)</label>
+        <input type="file" class="form-control" id="inputGroupFile01">
       </div>
     </div>
     <div class="container mb-5 form-group text-center">
-      <a href="{{ url('upload') }}"><button type="submit" class="btn btn-form btn-warning w-100 text-white">Proses Selanjutnya ></button></a>
+      <a href="#"><button type="submit" class="btn btn-form btn-warning w-100 text-white">Submit</button></a>
     </div>
   </main>
   <!-- End main -->
